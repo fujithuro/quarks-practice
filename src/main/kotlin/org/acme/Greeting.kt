b79497b2b25e8302f82b50618527ed5dc4cd892f
fujita_kyohei <fujita_kyohei@trial-net.co.jp>
2025-01-14 10:42:36 +0900

    fix: PanacheEntityを使わなくなったので、一意に識別するためのプロパティが必要

--- src/main/kotlin/org/acme/Greeting.kt
+++ src/main/kotlin/org/acme/Greeting.kt
@@ -1,8 +1,13 @@
 package org.acme
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 
 @Entity
 class Greeting {
+    @Id
+    @GeneratedValue
+    var id: Long? = null
     lateinit var name: String
 }
